546fab734ea304ec45ed66d7561b04e63fed55bb
user382232622 <2596397257068179-user382232622@noreply.coze.cn>
2026-06-04 14:12:18 +0800

    style: LOGO图片改为stretch模式适配手机端显示
    Coze-Commit-Type: user
    Coze-User-ID: 2596397257068179
    Coze-Conversation-ID: 5692465

--- client/screens/dashboard/index.tsx
+++ client/screens/dashboard/index.tsx
@@ -340,7 +340,7 @@ const styles = StyleSheet.create({
   logo: {
     width: 220,
     height: 88,
-    resizeMode: 'cover',
+    resizeMode: 'stretch',
     backgroundColor: '#F0F0F3',
     borderRadius: 8,
   },
